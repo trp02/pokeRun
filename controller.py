55b516c53390dd763b882470412e749cd4906ea0
TRP <tirth4ur@gmail.com>
2023-03-28 16:16:38 -0400

    added player character and jumping mechanic

--- controller.py
+++ controller.py
@@ -1,7 +1,8 @@
 import pygame, sys, math, random
-from model import Model, obstacle1, obstacle2
+from model import Model, obstacle1, obstacle2, character
 SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 1300, 675
 FLOOR = 510
+CHARFLOOR = 424
 #helper variables
 bg = 0
 tiles = 0
@@ -15,13 +16,15 @@ class Controller:
     
         self.trigger = pygame.USEREVENT + 2
         pygame.time.set_timer(self.trigger, random.randrange(2000, 3500))
-    
+        
     def startGame(self):
         #game quit
         #can only happen after view has been initalized I guess
         global bg, tiles
         bg = pygame.image.load("assets/background.png").convert()
         tiles = math.ceil(SCREEN_WIDTH / (bg.get_width())) + 1
+        
+        player = character(500, 423, 86, 100)
         
         while True:
             for event in self.view.getEvents():
@@ -34,24 +37,46 @@ class Controller:
                     #chooses random obstacle to create
                     rand = random.randint(1,2)
                     if rand == 1:
-                        self.obstacles.append(obstacle1(1350, FLOOR-60, 115, 60))
+                        self.obstacles.append(obstacle1(1350, FLOOR-52, 100, 52))
                     elif(rand == 2):
-                        self.obstacles.append(obstacle2(1350, FLOOR-60, 115, 60))
-                        
-                    
+                        self.obstacles.append(obstacle2(1350, FLOOR-52, 100, 52))
             
             self.moveObstacles()
-            
             self.updateBackground()
+            
+            keys = self.view.getPressed()
+            if keys[pygame.K_SPACE]:
+                player.jump = True
+            
+            if(player.jump): 
+                self.movePlayer(player)
+            
+            
             #blits all obstacles
             for obs in self.obstacles:
                 obsData = obs.getImgInfo()
                 self.view.blitImg(obsData[0], obsData[1], obsData[2])
                 self.view.drawRect((255,0,0), obs.getHitbox())
         
+            #blits player
+            pData = player.getImgInfo()
+            self.view.blitImg(pData[0], pData[1], pData[2])
+            self.view.drawRect((255,0,0), player.getHitbox())
+                
             self.view.update()
             self.clock.tick(60)
     
+    
+    #for player controlled movements
+    def movePlayer(self, player):
+
+        player.y -= player.velocity
+        player.velocity -= .2
+            
+        if player.velocity < -10:
+            player.jump = False
+            player.velocity = 10
+        
     def moveObstacles(self):
         #keeps obstacles moving along with the background and removes once they go off screen
         obstacles_to_remove = []
--- model.py
+++ model.py
@@ -8,9 +8,10 @@ class Model:
         self.y = y
         self.width = width
         self.height = height
+        self.jump = False
+        self.velocity = 10
+        
 
-    def getHitbox(self):
-        return (self.x + 3, self.y + 3, self.width - 3, self.height - 3)
     
 class obstacle1(Model):
     
@@ -19,9 +20,37 @@ class obstacle1(Model):
     def getImgInfo(self):
         return (self.img, self.x, self.y)
     
+    def getHitbox(self):
+        return (self.x + 8, self.y + 3, self.width - 10, self.height - 3)
+    
 class obstacle2(Model):
 
     img = pygame.image.load("assets/spike2.png")
     #returns image and x, y coords 
     def getImgInfo(self):
         return (self.img, self.x, self.y)
+    def getHitbox(self):
+        return (self.x + 3, self.y + 3, self.width - 3, self.height - 3)
+class character(Model):
+    
+    #walking animation
+    charwalk = [pygame.image.load("assets/charmander/walk1.png"), pygame.image.load("assets/charmander/walk2.png"), pygame.image.load("assets/charmander/walk3.png"), pygame.image.load("assets/charmander/walk4.png")]
+    walk = 0
+    animation_timer = 0
+    animation_delay = 20 # Animation delay 
+    current_time = 0
+    def getImgInfo(self):
+        self.current_time += 1
+        if self.current_time - self.animation_timer > self.animation_delay:
+            self.walk += 1
+            # Reset the walk counter if it exceeds the length of the charwalk list
+            if self.walk >= len(self.charwalk):
+                self.walk = 0
+            # Update the animation timer
+            self.animation_timer = self.current_time
+        # return image info
+        return (self.charwalk[self.walk], self.x, self.y)
+    
+    #gets hitbox for character. A little smaller than what you see to allow for some on-the-edge gameplay
+    def getHitbox(self):
+        return (self.x + 13, self.y + 25, self.width - 30, self.height - 40)
