4460570cc074a11d7900d1f19384afd0b597664a
TRP <tirth4ur@gmail.com>
2023-04-14 23:21:32 -0400

    added loading screen and pacing adjustments

--- controller.py
+++ controller.py
@@ -14,26 +14,41 @@ class Controller:
         self.clock = pygame.time.Clock()
         self.obstacles = []
         self.lastCharChangeTime = time.time()
-
+        self.triggerDelay = 2500
+        self.triggerDelay2 = 2600
         self.trigger = pygame.USEREVENT + 2
-        pygame.time.set_timer(self.trigger, random.randrange(2000, 3500))
+        pygame.time.set_timer(self.trigger, random.randrange(self.triggerDelay, self.triggerDelay2))
+        self.clockspeed = 60
+        self.score = 0
         
     def startGame(self):
         #game quit
         #can only happen after view has been initalized I guess
         global bg, tiles, scroll
         bg = pygame.image.load("assets/background7.png").convert()
+        startImg = pygame.image.load("assets/loading/start.jpg")
         tiles = math.ceil(SCREEN_WIDTH / (bg.get_width())) + 1
         
         player = character(500, 410, 86, 100)
-        
+
+        input_received = False
+        while not input_received:
+            self.view.blitImg(startImg, 0, 0)
+            self.view.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    input_received = True
+                elif event.type == pygame.KEYDOWN:
+                    input_received = True
+                
         while True:
             
             for event in self.view.getEvents():
                 if event.type == pygame.QUIT:
                     self.view.endGame()
                     sys.exit()
-                    
+                
+                self.score += 1
                 
                 #generates new obstacle
                 if event.type == self.trigger:
@@ -47,20 +62,33 @@ class Controller:
                         self.obstacles.append(waterObs(1350, FLOOR-350, 94, 300))
                     elif(rand == 4):
                         self.obstacles.append(grassObs(1350, FLOOR-251, 94, 300))
+                    if self.clockspeed > 170:
+                        rand = random.randint(1,4)
+                        buf = 450
+                        if rand == 1:
+                            self.obstacles.append(snorObs(1350 + buf, FLOOR-52, 100, 52))
+                        elif(rand == 2):
+                            self.obstacles.append(fireObs(1350+ buf, FLOOR-280, 94, 300))
+                        elif(rand == 3):
+                            self.obstacles.append(waterObs(1350 +buf, FLOOR-350, 94, 300))
+                        elif(rand == 4):
+                            self.obstacles.append(grassObs(1350 +buf, FLOOR-251, 94, 300))
                         
             if player.health < 2.5:
                 rand = random.randint(1, 50)
                 if rand == 5:
-                    x = self.obstacles[len(self.obstacles) -1].x 
-                    y = random.randint(200, 400)
-                    t = hPack(x + 220, y, 40, 40)
-                    rand = random.randint(0,2)
-                    t.pack = rand
-                    self.obstacles.append(t)
-                    for i in range(len(self.obstacles)):
-                        if (i < len(self.obstacles) - 1) and type(self.obstacles[i]) is hPack:
-                            self.obstacles.remove(t)
-                                    
+                    if self.obstacles:
+                        x = self.obstacles[len(self.obstacles) -1].x
+                        if x + 220 > 1350:
+                            y = random.randint(200, 400)
+                            t = hPack(x + 220, y, 40, 40)
+                            rand = random.randint(0,2)
+                            t.pack = rand
+                            self.obstacles.append(t)
+                            for i in range(len(self.obstacles)):
+                                if (i < len(self.obstacles) - 1) and type(self.obstacles[i]) is hPack:
+                                    self.obstacles.remove(t)
+                                            
             if player.health <= 0:
                 player.jump = True
                 self.movePlayer(player)
@@ -95,7 +123,7 @@ class Controller:
             for obs in self.obstacles:
                 obsData = obs.getImgInfo()
                 self.view.blitImg(obsData[0], obsData[1], obsData[2])
-                self.view.drawRect((255,0,0), obs.getHitbox())
+               # self.view.drawRect((255,0,0), obs.getHitbox())
                 if pygame.Rect.colliderect(playerHitbox, pygame.Rect(obs.getHitbox())):
                     if player.curChar == 0 and (type(obs) is fireObs):
                         player.health -= 0.5
@@ -126,10 +154,19 @@ class Controller:
             pData = player.getImgInfo()
             self.view.blitImg(pData[0], pData[1], pData[2])
           #  self.view.drawRect((255,0,0), player.getHitbox())
-                
+            self.updateClock()
             self.view.update()
-            self.clock.tick(60)
-    
+            self.clock.tick(self.clockspeed)
+            self.score += 1
+        print(self.score)
+    def updateClock(self):
+        if self.clockspeed > 100 and self.triggerDelay > 0:
+            self.triggerDelay -= 10
+            self.triggerDelay2 -= 10.8
+        if self.clockspeed < 180:
+            self.clockspeed += .05
+        
+        
     def drawHealth(self, player):
         packs = player.getHealth()
         x = 20
